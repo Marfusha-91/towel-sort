6c7bac0d93b0cd3e5c7c11a43fc05e4ed0780440
Marfusha-91 <71018868+Marfusha-91@users.noreply.github.com>
2021-03-11 00:03:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 module.exports = function towelSort (matrix) {
  
    const item = [];
+   if(!matrix) { return item; }
    item.sort(( a, b ) => a.ratingRevievs > b.ratingRevievs ? 1 : -1);
     return item
 }
