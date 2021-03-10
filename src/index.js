43590b434329f02698e7fae7569aeb082f90ed36
Marfusha-91 <71018868+Marfusha-91@users.noreply.github.com>
2021-03-10 22:18:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+    
+        const  length  = arr;
+        let minIndex;
+        for (let i = 0; i < length - 1; i++) {
+          minIndex = i;
+         for (let j = i; j < length; j++) {
+            if (compare(arr[minIndex], arr[j]) === Compare.BIGGER_THAN) {
+                minIndex = j;
+            }
+        }
+        if (i !== minIndex) {
+            swap(arr, i, minIndex);
+        }
+    }
+    return arr;
+}
+    
   return [];
 }
